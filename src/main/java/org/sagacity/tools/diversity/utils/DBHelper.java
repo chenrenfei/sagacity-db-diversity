4994d5c552d0517bafa50748423162ba3b0faa3a
zhongxuchen <zhongxuchen@gmail.com>
2019-09-20 09:28:16 +0800

    Update DBHelper.java

--- src/main/java/org/sagacity/tools/diversity/utils/DBHelper.java
+++ src/main/java/org/sagacity/tools/diversity/utils/DBHelper.java
@@ -7,11 +7,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
